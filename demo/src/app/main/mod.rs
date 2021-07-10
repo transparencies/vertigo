7b8ea86316664c2d7dd0422abbaf539f53fe04ca
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-07-10 15:46:00 +0200

    kolejny

--- demo/src/app/main/mod.rs
+++ demo/src/app/main/mod.rs
@@ -13,11 +13,12 @@ use spinner::spinner;
 #[derive(PartialEq)]
 pub struct Animacja {
     pub value: Value<u32>,
+    licznik_punktow: Value<u32>,
     _timer: DropResource,
 }
 
 impl Animacja {
-    pub fn new(root: &Dependencies, driver: &DomDriver) -> Animacja {
+    pub fn new(root: &Dependencies, driver: &DomDriver, licznik_punktow: Value<u32>) -> Computed<Animacja> {
         let value = root.new_value(0);
 
         let timer = {
@@ -29,10 +30,47 @@ impl Animacja {
             })
         };
 
-        Animacja { 
+        root.new_computed_from(Animacja { 
             value,
+            licznik_punktow,
             _timer: timer,
+        })
+    }
+
+    pub fn ustaw(&self, new_value: u32) {
+        let nowy_punkty = self.value.get_value();
+        let punkty = self.licznik_punktow.get_value();
+        self.licznik_punktow.set_value(*nowy_punkty + *punkty);
+
+        self.value.set_value(new_value);
+    }
+}
+
+fn animacja_rysuj(animacja: &Computed<Animacja>) -> VDomElement {
+    
+    let curr = animacja.get_value().value.get_value();
+    let curr = if *curr < 50 { *curr } else { 50 };
+
+    let mut out = Vec::new();
+
+    for i in 0..(curr + 1) {
+        out.push(format!("{} ", i));
+    }
+
+    let out_str = out.as_slice().join("");
+
+    let wyzeruj = {
+        let animacja = animacja.get_value();
+        move || {
+            animacja.ustaw(0);
         }
+    };
+
+    html! {
+        <div>
+            {out_str}
+            <button onClick={wyzeruj}>"wyzeruj"</button>
+        </div>
     }
 }
 
@@ -40,18 +78,25 @@ impl Animacja {
 pub struct MainState {
     driver: DomDriver,
     pub value: Value<u32>,
-    pub animacja: Value<Animacja>,
+    pub licznik_punktow: Value<u32>,
+    pub animacje: Vec<Computed<Animacja>>,
 }
 
 impl MainState {
     pub fn new(root: &Dependencies, driver: DomDriver) -> Computed<MainState> {
-        let startowa_animacja = Animacja::new(root, &driver);
-        let animacja = root.new_value(startowa_animacja);
+
+        let licznik_punktow = root.new_value(0);
+        let mut animacje = Vec::new();
+
+        for _ in 0..21 {
+            animacje.push(Animacja::new(root, &driver, licznik_punktow.clone()));
+        }
 
         root.new_computed_from(MainState {
             value: root.new_value(30),
             driver,
-            animacja,
+            licznik_punktow,
+            animacje,
         })
     }
 
@@ -64,7 +109,6 @@ impl MainState {
         let rr = self.value.get_value();
         self.value.set_value(*rr - 1);
     }
-
 }
 
 css_fn! { css_bg, "
@@ -136,16 +180,32 @@ pub fn main_render(state: &Computed<MainState>) -> VDomElement {
     let value2 = value * 2;
     let value_polowka = value / 2;
 
-    let curr = state.animacja.get_value().value.get_value();
-    let curr = *curr;
+    let mut animacje_html = Vec::new();
+
+    for animacja in state.animacje.iter() {
+        animacje_html.push(html!{
+            <component {animacja_rysuj} data={animacja.clone()} />
+        });
+    }
+
+    let licznik_punktow = state.licznik_punktow.get_value();
+    let punkty_string = format!("punktów = {}", *licznik_punktow);
 
     html! {
         <div aaa="one" bbb="two">
-            "Abudabi"
+            <div>
+                <div>
+                    { punkty_string.clone() }
+                </div>
+                <div>
+                { ..animacje_html }
+                </div>
+            </div>
+
+            <div>"Abudabi"</div>
             <div css={css_bg()}>
                 { wynik }
                 { spinner() }
-                { curr }
             </div>
             <div css={css_bg()} onClick={pomniejsz_liczbe}>
                 "Actual value = " { value } " Jak klikniesz, to zmniejszę dwa razy --> " { value_polowka }
@@ -161,6 +221,8 @@ pub fn main_render(state: &Computed<MainState>) -> VDomElement {
             </div>
             <button onClick={wyzeruj}>"Wyzeruj"</button>
             <button onClick={ustaw_sto}>"Ustaw stoo"</button>
+
+            { punkty_string }
         </div>
     }
 }
